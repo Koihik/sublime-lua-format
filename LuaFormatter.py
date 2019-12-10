dd91e6e0a87f65566510f059d094a60578b8807c
Floyda <Floyda@163.com>
2019-12-10 20:07:13 +0800

    refactor the sublime package to make it work

--- LuaFormatter.py
+++ LuaFormatter.py
@@ -1,26 +1,49 @@
-import os.path
+import os
 import sublime, sublime_plugin, sys
 import subprocess
+import shutil
+
+
+LUA_STYLE_NAME = "lua_style.lua-format"
+
+
+def plugin_loaded():
+    init_config_file()
+
+
+def init_config_file():
+    package_path = os.path.dirname(os.path.realpath(__file__))
+    config_src_path = os.path.join(package_path, LUA_STYLE_NAME)
+    config_dst_path = os.path.join(sublime.packages_path(), "User", LUA_STYLE_NAME)
+    if not os.path.exists(config_dst_path):
+        shutil.copyfile(config_src_path, config_dst_path)
+
 
 class LuaFormatCommand(sublime_plugin.TextCommand):
-  def run(self, edit, error=True, save=True):
-    self.filename = self.view.file_name()
-    self.fname = os.path.basename(self.filename)
-    region = sublime.Region(0, self.view.size())
+    def run(self, edit, error=True, save=True):
+        # check whether the lua files
+        suffix_setting = self.view.settings().get("syntax")
+        file_suffix = suffix_setting.split(".")[0]
+        if file_suffix[-3:].lower() != "lua":
+            return
 
-    settings = sublime.load_settings('LuaFormatter.sublime-settings')
-    config = settings.get('config_file')
+        # check whether filename is None
+        filename = self.view.file_name()
+        if filename is None:
+            return
 
-    src = self.view.substr(region)
-    command = os.path.join(sublime.packages_path(), 'LuaFormatter', 'bin', sys.platform, 'lua-format')
-    command = "'" + command + "' -si '" + self.filename + "'"
-    if config:
-        command = command + " -c " + config
+        # run lua-format
+        package_path = os.path.split(os.path.dirname(__file__))[1]
+        executable_path = os.path.join(
+            sublime.packages_path(), package_path, "bin", sys.platform, "lua-format"
+        )
 
-    process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    out = process.communicate(src.encode("utf-8"))
-    if out[0]:
-    	self.view.replace(edit, region, out[0].decode('utf8'))
-    if out[1]:
-    	msg = "Run lua-format error: \n" + out[1].decode('utf8')
-    	sublime.error_message(msg)
+        config_path = os.path.join(sublime.packages_path(), "User", LUA_STYLE_NAME)
+        process = subprocess.Popen([executable_path, filename, "-c", config_path])
+
+
+class LuaFormatOnPreSave(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        s = sublime.load_settings("LuaFormatter.sublime-settings")
+        if s.get("auto_format_on_save", False):
+            view.run_command("lua_format")
